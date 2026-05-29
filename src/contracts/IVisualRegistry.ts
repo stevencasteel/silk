ff75fca94417511fe74176cecb585ba500d7d383
Steven Casteel <stevencasteel@protonmail.com>
2026-05-28 19:07:21 -0500

    refactor: segregate visual registry and system context interfaces
    Split IVisualRegistry into IVisualQuery and IVisualRegistration, and updated SystemContext to expose segregated read/write interfaces.

--- src/contracts/IVisualRegistry.ts
+++ src/contracts/IVisualRegistry.ts
@@ -1,10 +1,15 @@
 import * as BABYLON from "@babylonjs/core";
 import { EntityId } from "../core/ecs/Entity";
 
-export interface IVisualRegistry {
+export interface IVisualQuery {
   getScene(): BABYLON.Scene | null;
   getTransformNode(id: EntityId): BABYLON.TransformNode | null;
+}
+
+export interface IVisualRegistration {
   registerTransformNode(id: EntityId, node: BABYLON.TransformNode): void;
   unregisterTransformNode(id: EntityId): void;
   registerShadowCaster(mesh: BABYLON.AbstractMesh): void;
 }
+
+export interface IVisualRegistry extends IVisualQuery, IVisualRegistration {}
--- src/core/engine/SystemContext.ts
+++ src/core/engine/SystemContext.ts
@@ -2,10 +2,13 @@ import { EcsWorld } from "../ecs/EcsWorld";
 import { EventBroker } from "../events/EventBroker";
 import { CommandBus } from "../commands/CommandBus";
 import { EntityRefs } from "../ecs/EntityRefs";
-import { IVisualRegistry } from "../../contracts/IVisualRegistry";
+import { IVisualRegistry, IVisualQuery, IVisualRegistration } from "../../contracts/IVisualRegistry";
 import { StoreContainer } from "../ecs/StoreContainer";
 
 export class SystemContext {
+  public readonly visualQuery: IVisualQuery;
+  public readonly visualRegistration: IVisualRegistration;
+
   constructor(
     public readonly world: EcsWorld,
     public readonly broker: EventBroker,
@@ -13,5 +16,8 @@ export class SystemContext {
     public readonly refs: EntityRefs,
     public readonly visualRegistry: IVisualRegistry,
     public readonly stores: StoreContainer
-  ) {}
+  ) {
+    this.visualQuery = visualRegistry;
+    this.visualRegistration = visualRegistry;
+  }
 }
